1b860cdab7cebcc9a180386b613c83cd9ac525cd
lzlocki <lzlocki@poczta.fm>
2024-11-23 15:42:08 +0100

    first commit

--- wsb-jpa1/src/main/java/com/jpacourse/WsbJpaApplication.java
+++ wsb-jpa1/src/main/java/com/jpacourse/WsbJpaApplication.java
@@ -3,6 +3,9 @@ package com.jpacourse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// Lukasz
+
 @SpringBootApplication
 public class WsbJpaApplication {
 
